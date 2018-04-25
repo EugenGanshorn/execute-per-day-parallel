1c67eee5344917c82fffe77401ed57ad1a4b2256
Eugen Ganshorn <eugen.ganshorn@check24.de>
2018-04-25 16:14:21 +0200

    init

---
+++ execute-per-day-parallel.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+## Usage:
+## ./execute-per-day-parallel.sh {MAX_CHILDREN} {START_DATE} {END_DATE} {COMMAND}
+
+if [ "$#" -le 4 ]; then
+    echo "Illegal number of parameters"
+    exit -1
+fi
+
+if [ "$1" -le "0" ] || [ "$1" -gt "100" ]; then
+    echo "Illegal number of children"
+    exit -1
+fi
+
+max_children=$1
+start_date=$(date -I -d "$2") || exit -1
+end_date=$(date -I -d "$3") || exit -1
+command=${@:4}
+
+if [ "$(date -d "$start_date" +%Y%m%d)" -ge "$(date -d "$end_date" +%Y%m%d)" ]; then
+    echo "end date is before start date"
+    exit -1
+fi
+
+function parallel {
+    local time1=$(date +"%H:%M:%S")
+    local time2=""
+    
+    echo "starting $@ ($time1)..."
+    "$@" && time2=$(date +"%H:%M:%S") && echo "finishing $@ ($time1 -- $time2)..." &
+    
+    local children=$(ps -eo ppid | grep -w $my_pid | wc -l)
+    children=$((children-1))
+
+    while [[ $children -ge $max_children ]]; do
+        echo "$children of $max_children processes running, wait..."
+        wait -n
+        sleep 1
+
+        children=$(ps -eo ppid | grep -w $my_pid | wc -l)
+        children=$((children-1))
+    done
+}
+
+my_pid=$$
+echo "my pid: $my_pid"
+
+while [ "$(date -d "$start_date" +%Y%m%d)" -lt "$(date -d "$end_date" +%Y%m%d)" ]; do
+    printf -v exec_command "$command" "$start_date" "$(date -I -d "$start_date + 1 day")"
+    parallel $exec_command
+    sleep 1
+    start_date=$(date -I -d "$start_date + 1 day")
+done
+
+wait
